97d07d9cf6d320466cd37b1c74dda43a8275b0c4
geraudazangue <52470818+geraudazangue@users.noreply.github.com>
2019-07-06 01:07:15 +0200

    Adding exception

--- Count_Export_SKU_XML_GLOBAL.py
+++ Count_Export_SKU_XML_GLOBAL.py
@@ -17,25 +17,28 @@ logging.basicConfig(filename='app.log', filemode='w', level=logging.DEBUG, forma
 count2=0
 value = []
 
-for p in  Path('C:/RTC/Scripts & Tools & Files/Python/COUNT SKU PY').glob('**/*.xml'):
-    if p.is_file():
-        xml = etree.parse(str(p))
-        count = len(xml.findall(".//*[A0001]"))
-        print(count)
+try:
+    for p in  Path('C:/RTC/Scripts & Tools & Files/Python/COUNT SKU PY').glob('**/*.xml'):
+        if p.is_file():
+            xml = etree.parse(str(p))
+            count = len(xml.findall(".//*[A0001]"))
+            print(count)
 
-        for nb in xml.findall('.//PRODUCT'):
-            s_sku = nb.find("A0001").text
-            value.append(s_sku)
+            for nb in xml.findall('.//PRODUCT'):
+                s_sku = nb.find("A0001").text
+                value.append(s_sku)
 
-    count2 = count + count2
-    logging.info('%s Nb sku', count2)
-print(count2)
+        count2 = count + count2
+        logging.info('%s Nb sku', count2)
+    print(count2)
 
-d = {'SKU':value}
-df = pd.DataFrame(d)
-datestring = datetime.strftime(datetime.now(), ' %Y%m%d_%H%M%S')
-export_excel = df.to_excel (r"C:/RTC/Scripts & Tools & Files/Python/COUNT SKU PY/{0}".format('export_NB_SKU' + datestring + '.xlsx'), index = None, header=True)
+    d = {'SKU':value}
+    df = pd.DataFrame(d)
+    datestring = datetime.strftime(datetime.now(), ' %Y%m%d_%H%M%S')
+    export_excel = df.to_excel (r"C:/RTC/Scripts & Tools & Files/Python/COUNT SKU PY/{0}".format('export_NB_SKU' + datestring + '.xlsx'), index = None, header=True)
 
+except Exception as e:
+  logging.exception("Exception occurred")
 #a= glob.glob('C:/Users/Gael/Desktop/TESTDOSSIER/*.xml')
 #a = os.listdir('C:/Users/Gael/Desktop/CODE/vba')
 #b = os.path.realpath('a')
