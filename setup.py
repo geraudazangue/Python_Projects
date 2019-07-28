779aeacd2ab77999278c987d1f2ec4853f314eb6
geraudazangue <52470818+geraudazangue@users.noreply.github.com>
2019-07-28 09:17:57 +0200

    Create setup.py

---
+++ setup.py
@@ -0,0 +1,21 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="project_python",
+    version="0.0.1",
+    author="Geraud AZANGUE",
+    author_email="geraudnguetsebaudouin@gmail.com",
+    description="A small test package",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/pypa/sampleproject",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+)
