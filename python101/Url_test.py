5ca9ed61e714fd3a608ff3c624eb634a116d9c79
geraudazangue <52470818+geraudazangue@users.noreply.github.com>
2019-11-27 23:55:03 +0100

    Create Url_test.py

---
+++ python101/Url_test.py
@@ -0,0 +1,23 @@
+#pip install requests
+import requests
+response = requests.get('https://api.github.com')
+#response.status_code
+response.content
+response.text
+
+'''from requests.exceptions import HTTPError
+
+for url in ['https://api.github.com', 'https://api.github.com/invalid']:
+    try:
+        response = requests.get(url)
+
+        # If the response was successful, no Exception will be raised
+        response.raise_for_status()
+    except HTTPError as http_err:
+        print(f'HTTP error occurred: {http_err}')  # Python 3.6
+    except Exception as err:
+        print(f'Other error occurred: {err}')  # Python 3.6
+    else:
+        print('Success!')
+'''
+
