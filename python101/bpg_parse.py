f42ddfb4db2e3b18f2099bf244eb968eb47c1ec7
geraudazangue <52470818+geraudazangue@users.noreply.github.com>
2019-12-14 00:13:55 +0100

    Add files via upload

---
+++ python101/bpg_parse.py
@@ -0,0 +1,12 @@
+import pandas as pd
+
+#using cp437 for encoding instead of utf8
+with open("C:/Users/Gael/Desktop/CODE/RAW FILES/pipe dataset/result_contacts.txt", 'r', encoding='cp437') as f:
+    # get No of columns in each line
+    col_count = [ len(l.split("|")) for l in f.readlines() ]
+
+column_names = [i for i in range(0, max(col_count))]
+
+### Read csv
+df = pd.read_csv("C:/Users/Gael/Desktop/CODE/RAW FILES/pipe dataset/result_contacts.txt", header=None, delimiter="|", names=column_names, encoding = "utf-8")
+print(df.head())
