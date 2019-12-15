a8347bb7120f4d2e17d88e0cf1919fb2dc966fad
geraudazangue <52470818+geraudazangue@users.noreply.github.com>
2019-12-15 17:30:59 +0100

    Adding dtype

--- python101/bpg_parse.py
+++ python101/bpg_parse.py
@@ -1,6 +1,8 @@
 import pandas as pd
 
 #using cp437 for encoding instead of utf8
+#dtype={'col1': Str, 'col2': int64}
+
 with open("C:/Users/Gael/Desktop/CODE/RAW FILES/pipe dataset/result_contacts.txt", 'r', encoding='cp437') as f:
     # get No of columns in each line
     col_count = [ len(l.split("|")) for l in f.readlines() ]
