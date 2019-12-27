a6b5ef77afbaf3a4c03e3ec9a7f3d341e15bf9a5
geraudazangue <52470818+geraudazangue@users.noreply.github.com>
2019-12-27 01:32:43 +0100

    Update formatting_excel.py

--- python101/formatting_excel.py
+++ python101/formatting_excel.py
@@ -29,6 +29,30 @@ worksheet.merge_range('B4:D4', 'Merged Range', header_format)
 # Write the column headers with the defined format.
 for col_num, value in enumerate(df.columns.values):
     worksheet.write(0, col_num, value, header_format)
-
 # Close the Pandas Excel writer and output the Excel file.
 writer.save()
+
+
+######FILLING EXCEL FILE ########
+from openpyxl import load_workbook
+import glob
+import os
+
+directory = r'C:\Users\Gael\Desktop\CODE\python'
+all_files = glob.glob(os.path.join(directory, "*.xlsx))
+
+def forwardfill(file):
+	df_flat_file = pd.read_excel(file, sheet_names=1, skiprows=1)
+	df_filled_file = df_flat_file.ffill()
+	book = load_workbook(file)
+	writer = pd.ExcelWriter(file, engine='openpyxl')
+	writer.book = book
+	writer.sheets = book.worksheets
+	writer.sheets = {ws.title: ws for ws in book.worksheets}
+	
+	for sheetname in writer.sheets:
+		df_filled_file.to_excel(writer, sheet_name=sheetname, startrow=1, startcol=0, index=False, header=False)
+	writer.save()
+
+for path in all_files:
+	forwardfill(path)
