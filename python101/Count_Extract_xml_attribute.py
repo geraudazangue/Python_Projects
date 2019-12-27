6c1d5871d30f1fd3ac5bee7caa488dbbf1e40c68
geraudazangue <52470818+geraudazangue@users.noreply.github.com>
2019-12-27 01:04:01 +0100

    Update and rename Count_xml_attribute.py to Count_Extract_xml_attribute.py

--- python101/Count_Extract_xml_attribute.py
+++ python101/Count_Extract_xml_attribute.py
@@ -18,24 +18,34 @@ count2=0
 value = []
 
 try:
+    #######Solution 1###############
     for p in  Path('C:/RTC/Scripts & Tools & Files/Python/COUNT SKU PY').glob('**/*.xml'):
         if p.is_file():
             xml = etree.parse(str(p))
             count = len(xml.findall(".//*[A0001]"))
             print(count)
-
             for nb in xml.findall('.//PRODUCT'):
                 s_sku = nb.find("A0001").text
                 value.append(s_sku)
-
         count2 = count + count2
         logging.info('%s Nb sku', count2)
     print(count2)
+ #######SOLUTION 2###############  
+test_list = [etree.parse(str(p)) for p in Path('C:/RTC/Scripts & Tools & Files/Python/COUNT SKU PY').glob('**/*.xml')
+                                 if p.is_file()]
+def find_sku(xml):
+    list_find_all_sku = [sku_tree for sku_tree in xml.findall('.//PRODUCT')]
+    sku_value_list = [sku.find("A0001").text for sku in list_find_all_sku]
+    return(sku_value_list)
 
-    d = {'SKU':value}
-    df = pd.DataFrame(d)
-    datestring = datetime.strftime(datetime.now(), ' %Y%m%d_%H%M%S')
-    export_excel = df.to_excel (r"C:/RTC/Scripts & Tools & Files/Python/COUNT SKU PY/{0}".format('export_NB_SKU' + datestring + '.xlsx'), index = None, header=True)
+for total in test_list:
+    value2.extend(sku_find(total))
+print(len(value2))
+
+d = {'SKU':value}
+df = pd.DataFrame(d)
+datestring = datetime.strftime(datetime.now(), ' %Y%m%d_%H%M%S')
+export_excel = df.to_excel (r"C:/RTC/Scripts & Tools & Files/Python/COUNT SKU PY/{0}".format('export_NB_SKU' + datestring + '.xlsx'), index = None, header=True)
 
 except Exception as e:
   logging.exception("Exception occurred")
