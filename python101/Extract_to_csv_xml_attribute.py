9c416cf2ae1d06996b8b1302bbbb67e77f8894d1
geraudazangue <geraudnguetsebadouin@gmail.com>
2020-01-26 14:51:53 +0100

    adding space

--- python101/Extract_to_csv_xml_attribute.py
+++ python101/Extract_to_csv_xml_attribute.py
@@ -15,7 +15,7 @@ list_of_attribute_value = []
 Attribute_to_extract = ['A0001','A9109','A9441','A1358','A1359','A9479','A9480','A9481','A9965','A9369']
 t= 0
 def parser_xml(tags, path):
-    tree = etree.iterparse(str(path))  
+    tree = etree.iterparse(str(path))
     for event, node in tree:
         if node.tag == 'A0001':
             sku = node.text
@@ -28,7 +28,7 @@ def parser_xml(tags, path):
         if node.tag in tags:
             yield [myocc, node.tag, node.text]
             myocc = newmyocc
-			
+
 #solution 1:
 list_of_generator = [parser_xml(Attribute_to_extract,file) for file in Path('C:/RTC/Scripts & Tools & Files/Python/COUNT SKU PY').glob('**/*.xml')
 															if file.is_file()]
@@ -36,7 +36,7 @@ for generator in list_of_generator:
     for  attribute_value in generator:
 		list_of_attribute_value.append(attribute_value)
 
- #solution 2:     
+ #solution 2:
 list_of_attribute_value2 = list(chain.from_iterable(parser_xml(Attribute_to_extract,file) for file in Path('C:/RTC/Scripts & Tools & Files/Python/COUNT SKU PY').glob('**/*.xml')))
 #print(list_of_attribute_value2)
 df_attributes_value = pd.DataFrame(list_of_attribute_value2, columns=['myocc','Attribute','Value'])
@@ -45,7 +45,6 @@ df_attributes_value = pd.DataFrame(list_of_attribute_value2, columns=['myocc','A
 df_prefinal_attribute_extract = df_attributes_value.pivot(index="myocc", columns='Attribute', values='Value')
 print(df_prefinal_attribute_extract)
 export_excel = df_prefinal_attribute_extract.to_excel (r"C:/RTC/Scripts & Tools & Files/Python/COUNT SKU PY/{0}".format('EXPORT_ELIX2SAP' + datestring + '.xlsx'), index = None, header=True)
-End =time.time()
-
+End = time.time()
 Execution_time = End - start
 print(f'{Execution_time} secs')
