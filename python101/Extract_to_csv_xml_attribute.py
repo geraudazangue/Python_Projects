140d4323763ee25351a7e2d55df3754e654b7a76
Geraud AZANGUE NGUETSE <Geraud.AZANGUE.NGUETSE@ibm.com>
2020-02-07 14:19:27 +0100

    adding new loop

--- python101/Extract_to_csv_xml_attribute.py
+++ python101/Extract_to_csv_xml_attribute.py
@@ -13,7 +13,22 @@ start = time.time()
 datestring = datetime.strftime(datetime.now(), '%Y%m%d_%H%M%S')
 list_of_attribute_value = []
 Attribute_to_extract = ['A0001','A9109','A9441','A1358','A1359','A9479','A9480','A9481','A9965','A9369']
-t= 0
+
+for path in Path('C:/RTC/Scripts & Tools & Files/Python/COUNT SKU PY').glob('**/*.xml'):
+    tree = etree.iterparse(str(path))
+    for event, node in tree:
+            if node.tag == 'A0001':
+                sku = node.text
+                t = 0
+                myocc = ''.join([sku,'-',str(t)])
+                newmyocc = ''.join([sku,'-',str(t)])
+            if node.tag == Attribute_to_extract[-1]:
+                t = t + 1
+                newmyocc = ''.join([sku,'-',str(t)])
+            if node.tag in Attribute_to_extract:
+                list_of_attribute_value.append([myocc, node.tag, node.text])
+                myocc = newmyocc
+#solution 2: much slower
 def parser_xml(tags, path):
     tree = etree.iterparse(str(path))
     for event, node in tree:
@@ -29,14 +44,6 @@ def parser_xml(tags, path):
             yield [myocc, node.tag, node.text]
             myocc = newmyocc
 
-#solution 1:
-list_of_generator = [parser_xml(Attribute_to_extract,file) for file in Path('C:/RTC/Scripts & Tools & Files/Python/COUNT SKU PY').glob('**/*.xml')
-															if file.is_file()]
-for generator in list_of_generator:
-    for  attribute_value in generator:
-		list_of_attribute_value.append(attribute_value)
-
- #solution 2:
 list_of_attribute_value2 = list(chain.from_iterable(parser_xml(Attribute_to_extract,file)
                                 for file in Path('C:/RTC/Scripts & Tools & Files/Python/COUNT SKU PY').glob('**/*.xml')))
 #print(list_of_attribute_value2)
